fc83e3b0fd953c21411ceca6de95febf3acbdda9
elenadimitrova <elena@arenabg.com>
2022-08-01 10:59:30 +0300

    Add tests for ensuring cold storage slots
    forge install: forge-std

---
+++ contracts/IMinter.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLINCENCEDs
+pragma solidity ^0.8.15;
+
+interface IMinter {
+  function totalQuantity() external returns (uint256);
+}
--- tests/Proxies.sol
+++ tests/Proxies.sol
@@ -5,8 +5,10 @@ import "../contracts/MinimalProxyFactory.sol";
 import "../contracts/MintImplementation.sol";
 import "../contracts/MintModule.sol";
 import "../contracts/RouterProxy.sol";
+import "../contracts/IMinter.sol";
+import "forge-std/Test.sol";
 
-contract Proxies_test {
+contract Proxies_test is Test {
 
     function testMintUsingMinimalProxyAndModule() public {
       MinimalProxyFactory minimalProxyFactory = new MinimalProxyFactory();
@@ -14,15 +16,29 @@ contract Proxies_test {
 
       address proxyAddress = minimalProxyFactory.create(address(minterImplementation));
 
+      // Check the total quantity is 0 before unit test
+      uint256 totalQuantity = IMinter(proxyAddress).totalQuantity();
+      assertEq(totalQuantity, 0);
+
       MintModule minterModule = new MintModule();
       minterModule.mint(proxyAddress, 1);
+
+      totalQuantity = IMinter(proxyAddress).totalQuantity();
+      assertEq(totalQuantity, 1);
     }
 
     function testMintUsingRouterProxyAndImplementation() public {
       MintImplementation minterImplementation = new MintImplementation();
       RouterProxy routerProxy = new RouterProxy(address(minterImplementation));
 
+      // Check the total quantity is 0 before unit test
+      uint256 totalQuantity = IMinter(address(routerProxy)).totalQuantity();
+      assertEq(totalQuantity, 0);
+
       MintImplementation minter = MintImplementation(address(routerProxy));
       minter.mint(1);
+
+      totalQuantity = IMinter(address(routerProxy)).totalQuantity();
+      assertEq(totalQuantity, 1);
     }
 }
